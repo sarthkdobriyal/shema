eaa0d5bd69c36cb86a2e94ef70d91480f0b422c7
sarthkdobriyal <dobriyalsarthaksa.8@gmail.com>
2026-07-02 16:30:15 +0530

    feat: add home page featured categories section

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+interface FeaturedCategory {
+  label: string;
+  imageUrl: string;
+  routerLink: string;
+}
+
 @Component({
   selector: 'app-home',
   imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  featuredCategories: FeaturedCategory[] = [
+    {
+      label: 'Men',
+      imageUrl: 'https://images.unsplash.com/photo-1516257984-b1b4d707412e?w=600',
+      routerLink: '/men',
+    },
+    {
+      label: 'Women',
+      imageUrl: 'https://images.unsplash.com/photo-1490481651871-ab68de25d43d?w=600',
+      routerLink: '/women',
+    },
+  ];
+}
