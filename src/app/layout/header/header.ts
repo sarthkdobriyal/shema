c577458669536ddc013d67c476fb2b2441ce17d5
sarthkdobriyal <dobriyalsarthaksa.8@gmail.com>
2026-07-08 11:09:40 +0530

    feat: enhance layout and responsiveness across multiple components

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -7,4 +7,14 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  isMenuOpen = signal(false);
+
+  toggleMenu() {
+    this.isMenuOpen.update((open) => !open);
+  }
+
+  closeMenu() {
+    this.isMenuOpen.set(false);
+  }
+}
